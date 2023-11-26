19aab13d62992670dfdb05a06d26d27bcde42aa3
sshinnee <samantha.wongsn@gmail.com>
2023-11-26 17:02:00 +0800

    start project

---
+++ nodejs/server.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+
+async function main() {
+    const uri = 'mongodb+srv://<username>:<password>@<your-cluster-url>/?retryWrites=true&w=majority'
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect()
+
+        const admin = client.db('admin')
+
+        await listDatabases(admin)
+    
+    } catch (e) {
+        console.error(e)
+    } finally {
+        await client.close()
+    }
+
+}
+
+async function listDatabases(admin) {
+    const databasesList = await admin.command(
+        {
+            listDatabases: 1
+        }
+    )
+ 
+    console.log("Databases:")
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`))
+}
+
+main().catch(console.error)
